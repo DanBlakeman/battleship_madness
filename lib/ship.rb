c7fdb588dd3d7cfdd828d37236cb27dc23c546ea
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-25 14:15:06 +0000

    Implemented Ship class

---
+++ lib/ship.rb
@@ -0,0 +1,16 @@
+class Ship
+  attr_reader :size, :hits
+
+  def initialize(size = 1)
+    @size = size
+    @hits = 0
+  end
+
+  def hit
+    @hits += 1
+  end
+
+  def sunk?
+    hits >= size
+  end
+end
